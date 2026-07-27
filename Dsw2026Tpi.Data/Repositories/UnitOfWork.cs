e6edf03b41165eb3312a2bdc41ebc5bd313feb23
Victoria Samira Ruiz <samiravictoriaruz26@gmail.com>
2026-07-27 09:29:27 -0300

    feat(data): implementar repositorio genérico y unit of work

---
+++ Dsw2026Tpi.Data/Repositories/UnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿using Dsw2026Tpi.Domain.Entities;
+using Dsw2026Tpi.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dsw2026Tpi.Data.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly DbContext _context;
+    private readonly Dictionary<string, object> _repositories;
+
+    public UnitOfWork(DbContext context)
+    {
+        _context = context ?? throw new ArgumentNullException(nameof(context));
+        _repositories = new Dictionary<string, object>();
+    }
+
+    public IRepository<T> Repository<T>() where T : EntityBase
+    {
+        var typeName = typeof(T).Name;
+
+        if (!_repositories.ContainsKey(typeName))
+        {
+            var repositoryInstance = new Repository<T>(_context);
+            _repositories.Add(typeName, repositoryInstance);
+        }
+
+        return (IRepository<T>)_repositories[typeName];
+    }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        return await _context.SaveChangesAsync();
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
