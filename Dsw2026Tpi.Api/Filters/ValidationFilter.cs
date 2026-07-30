d970248ef4f5c996b5e86b7069871a47780b7e2a
Mariano <Mariano.Garcia1@alu.frt.utn.edu.ar>
2026-07-30 18:41:06 -0300

    A07: mapeo y validacion

---
+++ Dsw2026Tpi.Api/Filters/ValidationFilter.cs
@@ -0,0 +1,34 @@
+﻿using AppValidationException = Dsw2026Tpi.CrossCutting.Exceptions.ValidationException;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Dsw2026Tpi.Api.Filters;
+
+public class ValidationFilter : IAsyncActionFilter
+{
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        foreach (var argument in context.ActionArguments.Values)
+        {
+            if (argument is null) continue;
+
+            var argumentType = argument.GetType();
+            var validatorType = typeof(IValidator<>).MakeGenericType(argumentType);
+
+            if (context.HttpContext.RequestServices.GetService(validatorType) is IValidator validator)
+            {
+                var validationContext = new ValidationContext<object>(argument);
+                var result = await validator.ValidateAsync(validationContext);
+
+                if (!result.IsValid)
+                {
+                    var exception = new AppValidationException();
+                    exception.WithDetail(result.Errors.Select(e => (e.PropertyName, e.ErrorMessage)));
+                    throw exception;
+                }
+            }
+        }
+
+        await next();
+    }
+}
---
+++ Dsw2026Tpi.Application/Validators/AppointmentRequestValidator.cs
@@ -0,0 +1,31 @@
+﻿using Dsw2026Tpi.Application.Dtos;
+using Dsw2026Tpi.Domain.Entities;
+using Dsw2026Tpi.Domain.Interfaces;
+using FluentValidation;
+
+namespace Dsw2026Tpi.Application.Validators;
+
+public class AppointmentRequestValidator : AbstractValidator<AppointmentModel.Request>
+{
+    public AppointmentRequestValidator(IUnitOfWork unitOfWork)
+    {
+        RuleFor(x => x.DoctorId)
+            .MustAsync(async (id, cancellation) =>
+            {
+                var doctor = await unitOfWork.Repository<Doctor>().GetByIdAsync(id);
+                return doctor is not null;
+            })
+            .WithMessage("El médico indicado no existe.");
+
+        RuleFor(x => x.AvailabilityId)
+            .NotEmpty().WithMessage("Debe indicar un horario disponible.");
+
+        RuleFor(x => x.Patient.Dni)
+            .Must(dni => dni.ToString().Length is >= 7 and <= 10)
+            .WithMessage("El DNI debe tener entre 7 y 10 dígitos.");
+
+        RuleFor(x => x.Reason)
+            .NotEmpty().WithMessage("El motivo es obligatorio.")
+            .MinimumLength(5).WithMessage("El motivo debe tener al menos 5 caracteres.");
+    }
+}
