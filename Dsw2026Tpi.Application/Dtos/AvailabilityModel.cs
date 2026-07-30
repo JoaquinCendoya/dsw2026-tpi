1692de4e3f4699eacb90cbad179d68c458fc1bd6
JoaquinCendoya <joaquincendoya1@gmail.com>
2026-07-30 16:57:35 -0300

    A06: DTOs

---
+++ Dsw2026Tpi.Application/Dtos/AvailabilityModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Tpi.Application.Dtos
+{
+    public record AvailabilityModel
+    {
+        public record Request(Guid DoctorId, IEnumerable<DaySchedule> Days);
+
+        public record DaySchedule(string Day, string StartTime, string EndTime);
+    }
+}
