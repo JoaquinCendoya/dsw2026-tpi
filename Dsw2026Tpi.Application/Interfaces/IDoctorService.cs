80e4960471eeb02396c4e7f52ff2a2f9ebc40aef
JoaquinCendoya <joaquincendoya1@gmail.com>
2026-07-31 20:07:39 -0300

    AO8 - Cambios 31/7

--- Dsw2026Tpi.Application/Interfaces/IDoctorService.cs
+++ Dsw2026Tpi.Application/Interfaces/IDoctorService.cs
@@ -7,7 +7,7 @@ public interface IDoctorService
 {
     Task<Pagination<DoctorModel.Response>> GetAll(int pageSize, int pageIndex, string? name = null);
     Task<DoctorModel.Response> Create(DoctorModel.Request request);
-    Task Update(Guid id, DoctorModel.Request request);
+    Task<DoctorModel.Response> Update(Guid id, DoctorModel.Request request);
     Task Delete(Guid id);
     Task<IEnumerable<DoctorModel.AvailabilityResponse>> GetAvailabilities(Guid id);
 }
--- Dsw2026Tpi.Application/Interfaces/ISpecialityService.cs
+++ Dsw2026Tpi.Application/Interfaces/ISpecialityService.cs
@@ -10,7 +10,7 @@ namespace Dsw2026Tpi.Application.Interfaces
     {
         Task<Pagination<SpecialityModel.Response>> GetAll(int pageSize, int pageIndex, string? name = null);
         Task<SpecialityModel.Response> Create(SpecialityModel.Request request);
-        Task Update(Guid id, SpecialityModel.Request request);
+        Task<SpecialityModel.Response> Update(Guid id, SpecialityModel.Request request);
         Task Delete(Guid id);
     }
 }
