d97941421edba913f40f39e111d357e956198781
MatiasAhumada <pagosptcmatias@gmail.com>
2026-08-01 18:03:53 -0300

    refactor: mueve SignInService a la capa Application

--- Dsw2026Tpi.Api/Configurations/DependencyInjectionConfigurationExtensions.cs
+++ Dsw2026Tpi.Api/Configurations/DependencyInjectionConfigurationExtensions.cs
@@ -1,6 +1,5 @@
-﻿using Dsw2026Tpi.Api.Services;
+﻿using Dsw2026Tpi.Application.Services;
 using Dsw2026Tpi.Application.Interfaces;
-using Dsw2026Tpi.Application.Services;
 using Dsw2026Tpi.Application.Validators;
 using Dsw2026Tpi.Data.Repositories;
 using Dsw2026Tpi.Domain.Interfaces;
