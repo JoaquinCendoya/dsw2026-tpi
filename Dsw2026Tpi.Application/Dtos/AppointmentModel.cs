e7c2df29984f65750c01bd00c0e861e37afe48f3
MatiasAhumada <pagosptcmatias@gmail.com>
2026-08-01 11:34:17 -0300

    fix: renombra AvailabilityId a AvailabilitySlotId segun contrato del enunciado

--- Dsw2026Tpi.Application/Dtos/AppointmentModel.cs
+++ Dsw2026Tpi.Application/Dtos/AppointmentModel.cs
@@ -6,7 +6,7 @@ namespace Dsw2026Tpi.Application.Dtos
 {
     public record AppointmentModel
     {
-        public record Request(Guid DoctorId, Guid AvailabilityId, PatientDto Patient, string Reason);
+        public record Request(Guid DoctorId, Guid AvailabilitySlotId, PatientDto Patient, string Reason);
 
         public record PatientDto(long Dni);
 
