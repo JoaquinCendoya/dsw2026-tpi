bd0fefd2d26b03cfb4b7c3f2ad530f4681691625
MatiasAhumada <pagosptcmatias@gmail.com>
2026-08-02 11:21:19 -0300

    refactor: cambiar name de posible null a requerido

--- Dsw2026Tpi.Application/Interfaces/ISpecialityService.cs
+++ Dsw2026Tpi.Application/Interfaces/ISpecialityService.cs
@@ -8,7 +8,7 @@ namespace Dsw2026Tpi.Application.Interfaces
 {
     public interface ISpecialityService
     {
-        Task<Pagination<SpecialityModel.Response>> GetAll(int pageSize, int pageIndex, string? name = null);
+        Task<Pagination<SpecialityModel.Response>> GetAll(int pageSize, int pageIndex, string name);
         Task<SpecialityModel.Response> Create(SpecialityModel.Request request);
         Task<SpecialityModel.Response> Update(Guid id, SpecialityModel.Request request);
         Task Delete(Guid id);
