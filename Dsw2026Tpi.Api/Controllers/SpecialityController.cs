25adf7e8196b3945c98530e9ee606b045c84dea7
Mariano <Mariano.Garcia1@alu.frt.utn.edu.ar>
2026-08-01 21:36:19 -0300

    feat:SpecialityController con endpoints CRUD

---
+++ Dsw2026Tpi.Api/Controllers/SpecialityController.cs
@@ -0,0 +1,56 @@
+﻿using Dsw2026Tpi.Application.Dtos;
+using Dsw2026Tpi.Application.Interfaces;
+using Dsw2026Tpi.CrossCutting.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dsw2026Tpi.Api.Controllers;
+
+[ApiController]
+[Route("api/specialties")]
+[Authorize(Policy = Policies.AdminPolicy)]
+public class SpecialityController : ControllerBase
+{
+    private readonly ISpecialityService _service;
+
+    public SpecialityController(ISpecialityService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAll([FromQuery] int pageSize, [FromQuery] int pageIndex, [FromQuery] string? name = null)
+    {
+        var specialities = await _service.GetAll(pageSize, pageIndex, name);
+        return Ok(specialities);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Create([FromBody] SpecialityModel.Request request)
+    {
+        var speciality = await _service.Create(request);
+        return CreatedAtAction(nameof(GetAll), new { id = speciality.Id }, speciality);
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Update(Guid id, [FromBody] SpecialityModel.Request request)
+    {
+        await _service.Update(id, request);
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        await _service.Delete(id);
+        return Ok();
+    }
+}
